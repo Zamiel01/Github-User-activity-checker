7e301730d02a0f29e84d8c5ede425005857e5b87
Zamiel01 <nkuinmbeng@gmail.com>
2025-10-28 10:04:52 +0100

    success in getting   user-activity

--- activity.py
+++ activity.py
@@ -1,7 +1,15 @@
-import requests
+import subprocess
 
 print('welcome to github user activity checker')
 
+#collecting user name
 user = str(input("enter the github username: "))
 
-response = requests.get()
+#using curl command to fetch user github activity
+cmd = ["curl", f"https://api.github.com/users/{user}/events"]
+
+result = subprocess.run(cmd, capture_output=True, text=True)
+
+print(result)
+
+ jjj
