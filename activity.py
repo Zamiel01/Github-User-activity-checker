45d4d534df3673cac74f7f0d912742711990ae3b
Zamiel01 <nkuinmbeng@gmail.com>
2025-11-04 11:13:48 +0100

    created new github activity checker using username

--- activity.py
+++ activity.py
@@ -1,15 +1,32 @@
-import subprocess
+import argparse
+import urllib.request
+import urllib.error
+import json
 
-print('welcome to github user activity checker')
+parser = argparse.ArgumentParser(description="Github User Activity CLI.")
+parser.add_argument('username', type=str, help="Enter github username")
+args = parser.parse_args()
 
-#collecting user name
-user = str(input("enter the github username: "))
+username = args.username
+api = f"https://api.github.com/users/{username}/events"
+profile_url = f"https://github.com/{username}"
 
-#using curl command to fetch user github activity
-cmd = ["curl", f"https://api.github.com/users/{user}/events"]
+# Check if username exists
+try:
+    urllib.request.urlopen(profile_url)
+except urllib.error.HTTPError as e:
+    if e.code == 404:
+        print("Username doesn't exist")
+        exit()
+    else:
+        print(f"Error: {e}")
+        exit()
 
-result = subprocess.run(cmd, capture_output=True, text=True)
-
-print(result)
-
- jjj
+# Fetch user events
+try:
+    response = urllib.request.urlopen(api)
+    data = response.read().decode('utf-8')
+    new_data = json.loads(data)
+    print(json.dumps(new_data, indent=4))
+except urllib.error.HTTPError as e:
+    print(f"Failed to fetch events: {e}")
